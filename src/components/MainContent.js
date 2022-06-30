445a3524ed56967f4df07d78456c17f120a60fe4
SanchitYadav2005 <sanchit2005yudavnashi@gmail.com>
2022-06-30 15:33:22 +0530

    done for the day

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,6 +3,9 @@ import React from "react";
 
 export default  function MainContent() {
     return(
-        <h1>This is my main content</h1>
+        <main className="main-content">
+            <h1 className="about">About</h1>
+            <p>I am a full stack developer with a prticular interset in making things simple and automating daily tasks. I try  to keep up with the security and best practices, and am always looking for new things to learn.</p>
+        </main>
     )
 }
